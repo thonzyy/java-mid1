1fef83f61f258d389805cf25123c965b59db09f5
thonzyy <136280932+thonzyy@users.noreply.github.com>
2024-06-08 19:49:45 +0900

    불변객체
    참조형 변수를 사용할 때는 객체를 공유하기 때문에 
    공유된 인스턴스에서 의도치 않게 공유값이 변경 될 수 있다.
    이러한 문제를 사이드 이펙트라고 하는데
    이 사이드 이펙트를 코드 상에서 찾아내기란 굉장히 시간이 많이 걸리는 작업이다.
    이 문제를 해결하기 위헤서는 
    참조형 변수마다 다른 인스턴스를 참조해서 값을 대입하게 만들 거나
    클래스를 설계할 때 필드에 final을 붙여서 생성될 때 값이 할당되고 
    이후에 값이 변경되지 않게끔 만들면 된다.
    
    하지만 이렇게 불변객체를 만들었을 경우에 
    불변객체의 값을 변경해야될 경우가 생긴다.
    이런 경우에는 withxxx()라는 메소드를 정의해서 
    리턴 값으로 불변객체를 새로 생성하여 던져주면 된다.
    이렇게 했을 경우에는 값이 변경될 때마다 메모리에 인스턴스가 생성되므로
    값이 빈번하게 변경되는 경우는 가변 클래스로 만드는 경우가 효과적일 수 있다.
    
    그래서 불변객체는 왜 배우는 걸까?
    자바에서 사용하는 String Interger등이 불변객체인데 
    나중에 더 깊은 내용을 알게 되면 차츰 알 수 있는 내용이다.
    불변객체에 더한 내용은 다음 섹션에서 다룬다.

---
+++ src/immutable/change/ImmutableObj.java
@@ -0,0 +1,23 @@
+package lang.immutable.change;
+
+public class ImmutableObj
+{
+    private final int value;
+
+    public ImmutableObj(int value)
+    {
+        this.value = value;
+    }
+    public ImmutableObj add(int addValue)
+    {
+        int result = value + addValue;
+        return new ImmutableObj(result);
+        //생성한 객체를 반환
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+}

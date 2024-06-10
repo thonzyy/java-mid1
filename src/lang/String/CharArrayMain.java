bee3540a62a8d2242cac8f2f941fd73f8f4586ed
thonzyy <136280932+thonzyy@users.noreply.github.com>
2024-06-10 09:12:52 +0900

    String 기본
    String 기본
    
    String이 없을 경우 
    Char 배열로 받아서 Pritn() 메소드로 뽑아낼 수 있다.
    코드가 복잡해지는 문제점이 생기고 때문에   
    간편하게 문자열을 다룰 수 있는 String class를 만듦.
    
    String은 하나의 인스턴스이다.
    인스턴스인 경우 다른 String을 선언했을 때 ==(동일성)로 비교를 하면 false
    equals()(논리적, 동등성 비교 )  는 true 
    왜냐하면 String 클래스 안에 따로 오버라이딩을 해놨기 때문이다.
    String은 리터럴로 선언, 초기화 할 수 있음.
    리터럴로 선언 시 ==비교 시 true로 나온다.   
    왜냐하면 힙영역에 String pool에 리터럴로 만든 인스턴스가 생성이 되고 같은 리터럴을 가진 변수들이 공용으로 사용하기 때문이다.
    그래서 같은 리터럴을 ==(동일성)비교시 true가 나온다.    
    
    Java 9 버전 이전에는 String 내부에서 문자열을 Char[]로 받았지만 
    Java 9버전 이후는 Byte[]로 받는다.
    
    String은 참조형 변수이고 하나의 인스턴스일 뿐만 아니라
    불변객체이다.
    불변객체는 내부 필드가 변경이 안 되고 
    한 번 만들어진 String은 값 변경이 안 됨(사이드이펙트 방지).
    그래서 Concat 등 String의 내부 메소드를 사용할 때 
    새로운 객체를 반환하는 것이 많음(withXXX()처럼).

---
+++ src/lang/String/CharArrayMain.java
@@ -0,0 +1,18 @@
+package lang.string;
+
+import java.sql.SQLOutput;
+
+public class CharArrayMain
+{
+    public static void main(String[] args)
+    {
+        char a = 'a';
+        System.out.println("a= "+a);
+
+        char[] charArr=new char[]{'a','b','c','d'};
+        System.out.println(charArr);
+
+        String str="hello";
+        System.out.println("str=  "+str);
+    }
+}
---
+++ src/lang/String/StringBasicMain.java
@@ -0,0 +1,16 @@
+package lang.string;
+
+import java.net.SocketTimeoutException;
+
+public class StringBasicMain
+{
+    public static void main(String[] args)
+    {
+        //int , boolean 프리미티브 타입
+        String str1="hello";
+        String str2 = new String("hello");
+        System.out.println("str1="+str1);
+        System.out.println("str2="+str2);
+
+    }
+}

6b429799ddac9ee24f1783c2bd146a440bf6abfe
thonzyy <136280932+thonzyy@users.noreply.github.com>
2024-06-14 17:49:47 +0900

    열거형 - ENUM ( 열거형 - Enum Type )
    자바는 타입 안전 열거형 패턴"(Type-Safe Enum Pattern)을 매우 편리하게 사용할 수 있는 열거형(Enum Type)을
    제공한다.
    쉽게 이야기해서 자바의 열거형은 앞서 배운 타입 안전 열거형 패턴을 쉽게 사용할 수 있도록 프로그래밍 언어에서 지원
    하는 것이다.
    영어인 enum 은 enumeration 의 줄임말인데, 번역하면 열거라는 뜻이고, 어떤 항목을 나열하는 것을 뜻한다.
    "Enumeration"은 일련의 명명된 상수들의 집합을 정의하는 것을 의미하며, 프로그래밍에서는 이러한 상수들을 사용하
    여 코드 내에서 미리 정의된 값들의 집합을 나타낸다.
    쉽게 이야기해서 회원의 등급은 상수로 정의한 BASIC , GOLD , DIAMOND 만 사용할 수 있다는 뜻이다.
    자바의 enum은 타입 안전성을 제공하고, 코드의 가독성을 높이며, 예상 가능한 값들의 집합을 표현하는 데 사용된다.
    
    
    실행 결과를 보면 상수들이 열거형으로 선언한 타입인 Grade 타입을 사용하는 것을 확인할 수 있다. 그리고 각
    각의 인스턴스도 서로 다른 것을 확인할 수 있다.
    참고로 열거형은 toString() 을 재정의 하기 때문에 참조값을 직접 확인할 수 없다. 참조값을 구하기 위해
    refValue() 를 만들었다.
    System.identityHashCode(grade) : 자바가 관리하는 객체의 참조값을 숫자로 반환한다.
    Integer.toHexString() : 숫자를 16진수로 변환, 우리가 일반적으로 확인하는 참조값은 16진수
    열거형도 클래스이다. 열거형을 제공하기 위해 제약이 추가된 클래스라 생각하면 된다.
    
    
    열거형(ENUM)의 장점
    타입 안정성 향상: 열거형은 사전에 정의된 상수들로만 구성되므로, 유효하지 않은 값이 입력될 가능성이 없다. 이
    런 경우 컴파일 오류가 발생한다.
    간결성 및 일관성: 열거형을 사용하면 코드가 더 간결하고 명확해지며, 데이터의 일관성이 보장된다.
    확장성: 새로운 회원 등급을 타입을 추가하고 싶을 때, ENUM에 새로운 상수를 추가하기만 하면 된다.
    참고
    열거형을 사용하는 경우 static import 를 적절하게 사용하면 더 읽기 좋은 코드를 만들 수 있다.

---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,17 @@
+package enumeration.ex3;
+
+
+
+public class ClassGradeEx3_2
+{
+    public static void main(String[] args)
+    {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+
+       /* Grade grade = new Grade(); enum 생성불가
+        int result = discountService.discount(, price);
+        System.out.println("newClassGrade의 할인 금액: " + result);*/
+
+    }
+}

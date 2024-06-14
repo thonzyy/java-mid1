a8b7cf9809b2003d12e27c2a656431e47397ba96
thonzyy <136280932+thonzyy@users.noreply.github.com>
2024-06-14 14:41:02 +0900

    열거형 - Enum ( 타입 안전 열거형 패턴 )
    타입 안전 열거형 패턴 - Type-Safe Enum Pattern
    지금까지 설명한 문제를 해결하기 위해 많은 개발자들이 오랜기간 고민하고 나온 결과가 바로 타입 안전 열거형 패턴이
    다.
    여기서 영어인 enum 은 enumeration 의 줄임말인데, 번역하면 열거라는 뜻이고, 어떤 항목을 나열하는 것을 뜻한
    다. 우리의 경우 회원 등급인 BASIC , GOLD , DIAMOND 를 나열하는 것이다. 여기서 중요한 것은 타입 안전 열거형 패
    턴을 사용하면 이렇게 나열한 항목만 사용할 수 있다는 것이 핵심이다. 나열한 항목이 아닌 것은 사용할 수 없다.
    쉽게 이야기해서 앞서본 String 처럼 아무런 문자열이나 다 사용할 수 있는 것이 아니라, 우리가 나열한 항목인
    BASIC , GOLD , DIAMOND 만 안전하게 사용할 수 있다는 것이다
    
    private 생성자를 사용해서 외부에서 ClassGrade 를 임의로 생성하지 못하게 막았다.
    private 생성자 덕분에 ClassGrade 의 인스턴스를 생성하는 것은 ClassGrade 클래스 내부에서만 할 수
    있다. 앞서 우리가 정의한 상수들은 ClassGrade 클래스 내부에서 ClassGrade 객체를 생성한다.
    이제 ClassGrade 인스턴스를 사용할 때는 ClassGrade 내부에 정의한 상수를 사용해야 한다. 그렇지 않으
    면 컴파일 오류가 발생한다.
    쉽게 이야기해서 ClassGrade 타입에 값을 전달할 때는 우리가 앞서 열거한 BASIC , GOLD , DIAMOND 상수
    만 사용할 수 있다.
    이렇게 private 생성자까지 사용하면 타입 안전 열거형 패턴을 완성할 수 있다.
    타입 안전 열거형 패턴"(Type-Safe Enum Pattern)의 장점
    타입 안정성 향상: 정해진 객체만 사용할 수 있기 때문에, 잘못된 값을 입력하는 문제를 근본적으로 방지할 수 있
    다.
    데이터 일관성: 정해진 객체만 사용하므로 데이터의 일관성이 보장된다.
    조금 더 자세히
    제한된 인스턴스 생성: 클래스는 사전에 정의된 몇 개의 인스턴스만 생성하고, 외부에서는 이 인스턴스들만 사용
    할 수 있도록 한다. 이를 통해 미리 정의된 값들만 사용하도록 보장한다.
    타입 안전성: 이 패턴을 사용하면, 잘못된 값이 할당되거나 사용되는 것을 컴파일 시점에 방지할 수 있다. 예를 들
    어, 특정 메서드가 특정 열거형 타입의 값을 요구한다면, 오직 그 타입의 인스턴스만 전달할 수 있다. 여기서는 메
    서드의 매개변수로 ClassGrade 를 사용하는 경우, 앞서 열거한 BASIC , GOLD , DIAMOND 만 사용할 수 있다.
    단점
    이 패턴을 구현하려면 다음과 같이 많은 코드를 작성해야 한다. 그리고 private 생성자를 추가하는 등 유의해야 하는
    부분들도 있다

---
+++ src/enumeration/ex2/DiscountService.java
@@ -0,0 +1,24 @@
+package enumeration.ex2;
+
+import enumeration.ex1.StringGrade;
+
+public class DiscountService
+{
+    public int discount(ClassGrade classGrade, int price)
+    {
+        int discountPercent = 0;
+
+        if (classGrade == ClassGrade.BASIC)
+            discountPercent = 10;
+         else if (classGrade == ClassGrade.GOLD)
+            discountPercent = 20;
+        else if (classGrade == ClassGrade.DIAMOND)
+            discountPercent = 30;
+        else
+            System.out.println("할인X");
+
+        // 10000 * (20 / 100) -> 2000원 할인
+        return price * discountPercent / 100;
+    }
+
+}
